b8e72a6defd11bfb8d2e9436b9e56e1d0e6d628c
Siddhant K <siddhantkumarupmanyu@gmail.com>
2022-06-04 17:24:29 +0530

    e2e test: capture input stream

--- e2e-test/EndToEndTest.cpp
+++ e2e-test/EndToEndTest.cpp
@@ -1,9 +1,29 @@
-#include <cstdlib>
+#include <array>
+#include <cstdio>
+#include <iostream>
+#include <memory>
+#include <ostream>
+#include <stdexcept>
 #include <string>
 
 int main() {
-    std::string app = "../src/app";
-    std::system(app.c_str());
+    std::string cmd = "../src/app";
+    std::array<char, 128> buffer;
+    std::string result;
+    std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd.c_str(), "r"),
+                                                  pclose);
+
+    if (!pipe) {
+        throw std::runtime_error("popen() failed!");
+    }
+    while (fgets(buffer.data(), buffer.size(), pipe.get())) {
+        result += buffer.data();
+    }
+
+    std::cout << result << std::endl;
 
     return 0;
 }
+
+
+// https://stackoverflow.com/a/478960
