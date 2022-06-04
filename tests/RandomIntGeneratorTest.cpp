379b587b1e0a674c323fd5d933887f6a4473259a
Siddhant K <siddhantkumarupmanyu@gmail.com>
2022-06-04 22:12:16 +0530

    RandomIntGeneratorTest: failing

--- tests/RandomIntGeneratorTest.cpp
+++ tests/RandomIntGeneratorTest.cpp
@@ -1,4 +1,11 @@
 #include "RandomIntGenerator.hpp"
 #include <catch2/catch.hpp>
 
-TEST_CASE("generateRandomInt") { auto intGenerator = RandomIntGenerator(); }
+TEST_CASE("generateRandomInt") {
+    auto intGenerator = RandomIntGenerator();
+    auto randomInt1 = intGenerator.generate();
+
+    auto randomInt2 = intGenerator.generate();
+
+    REQUIRE(randomInt1 != randomInt2);
+}
